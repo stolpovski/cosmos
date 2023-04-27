71ada280b942c0e0fdaf93b7194a8eca487c1b94
stolpovski <stolpovski@gmail.com>
2023-04-27 20:46:00 +0300

    demo 1

---
+++ Assets/LandPoint.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandPoint : MonoBehaviour
+{
+    GameObject astronaut;
+    GameObject starship;
+    bool ready = false;
+    float upForce = 0f;
+    float upForceTarget = 0f;
+    float forwardForce = 0f;
+    float forwardForceTarget = 0f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        astronaut = GameObject.Find("Astronaut");
+        starship = GameObject.Find("Starship");
+        rb = starship.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        upForce = Mathf.Lerp(upForce, upForceTarget, 10);
+        forwardForce = Mathf.Lerp(forwardForce, forwardForceTarget, 1f);
+        rb.AddRelativeForce(Vector3.up * upForce, ForceMode.Acceleration);
+        rb.AddRelativeForce(Vector3.forward * forwardForce, ForceMode.Acceleration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (ready && Input.GetKey(KeyCode.Return))
+        {
+            Debug.Log("Go!");
+            astronaut.SetActive(false);
+            starship.GetComponent<AudioSource>().Play();
+            upForceTarget = 25;
+            forwardForceTarget = 50;
+            
+
+        }
+    }
+    void OnTriggerEnter(Collider target)
+    {
+        if (target.transform.name == "Astronaut")
+        {
+            Debug.Log(target.transform.name + " enter");
+            ready = true;
+        }
+    }
+
+    void OnTriggerExit(Collider target)
+    {
+        if (target.transform.name == "Astronaut")
+        {
+            Debug.Log(target.transform.name + " exit");
+            ready = false;
+        }
+    }
+}
---
+++ Assets/Stylized_Astronaut/Character/AstronautPlayer.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+namespace AstronautPlayer
+{
+
+	public class AstronautPlayer : MonoBehaviour {
+
+		private Animator anim;
+		private CharacterController controller;
+
+		public float speed = 600.0f;
+		public float turnSpeed = 400.0f;
+		private Vector3 moveDirection = Vector3.zero;
+		public float gravity = 20.0f;
+
+		void Start () {
+			controller = GetComponent <CharacterController>();
+			anim = gameObject.GetComponentInChildren<Animator>();
+		}
+
+		void FixedUpdate (){
+			if (Input.GetKey ("w")) {
+				anim.SetInteger ("AnimationPar", 1);
+			}  else {
+				anim.SetInteger ("AnimationPar", 0);
+			}
+
+			if(controller.isGrounded){
+				float v = Input.GetAxis("Vertical");
+				if (v < 0) v = 0;
+				moveDirection = transform.forward * v * speed;
+			}
+
+			float turn = Input.GetAxis("Horizontal");
+			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+			controller.Move(moveDirection * Time.deltaTime);
+			moveDirection.y -= gravity * Time.deltaTime;
+		}
+	}
+}
