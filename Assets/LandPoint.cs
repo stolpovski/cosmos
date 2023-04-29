5928d5a09cedf05e0dc7715a437e4668f918291d
stolpovski <stolpovski@gmail.com>
2023-04-29 12:38:14 +0300

    scene changing

--- Assets/LandPoint.cs
+++ Assets/LandPoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LandPoint : MonoBehaviour
 {
@@ -45,9 +46,25 @@ public class LandPoint : MonoBehaviour
             upForceTarget = 25;
             forwardForceTarget = 50;
             
+            StartCoroutine(ExampleCoroutine());
+
 
         }
     }
+
+    IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(7);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
     void OnTriggerEnter(Collider target)
     {
         if (target.transform.name == "Astronaut")
